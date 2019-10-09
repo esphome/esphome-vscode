55a4e1456e97dbf1ad2dd33cd4f5da143c9dd811
Guillermo Ruffino <glm.net@gmail.com>
2019-10-08 23:49:38 -0300

    add include file support, checks all files at startup

--- src/extension.ts
+++ src/extension.ts
@@ -1,135 +1,155 @@
 import * as vscode from 'vscode';
-
 import * as WebSocket from "ws";
-
 import * as ChildProcess from "child_process";
-
-// This code is bad (I don't know typescript, and hate js :D)
-// It's only a very crude prototype, please do change the architecture where necessary.
-
-interface EsphomeRange {
-	document: string;
-	start_line: number;
-	start_col: number;
-	end_line: number;
-	end_col: number;
-}
-
-interface ValidationError {
-	range: EsphomeRange | null;
-	message: string;
-}
-
-const MESSAGE_READ_FILE = 'read_file';
-const MESSAGE_RESULT = 'result';
-
-interface MessageReadFile {
-	type: typeof MESSAGE_READ_FILE;
-	path: string;
-}
-
-interface YamlValidationError {
-	message: string;
-}
-
-interface MessageResult {
-	type: typeof MESSAGE_RESULT;
-	validation_errors: ValidationError[];
-	yaml_errors: YamlValidationError[];
-}
-
-type MessageTypes = MessageReadFile | MessageResult;
+import * as ESPHome from './esphome_types';
 
 export default class EsphomeProvider {
 	private output_channel !: vscode.OutputChannel;
 
 	private useWs = false; // if false use local esphome
-	private validating: string | null = null;
-
+	private validating: vscode.TextDocument | null = null;
 
 	private diagnosticCollection!: vscode.DiagnosticCollection;
 	private ws!: WebSocket;
 	private process!: ChildProcess.ChildProcess;
 
-	private handleMessage(msg: MessageTypes) {
-		switch (msg.type) {
-			case 'read_file': {
-				console.log(`Got read_file message ${msg.path}`);
-				const uri = vscode.Uri.file(msg.path);
-				vscode.workspace.openTextDocument(uri).then((document) => {
-					let text = document.getText();
-					this.sendMsg({
-						type: 'file_response',
-						content: text,
-					});
-				});
-				break;
+	private pendingValidation: vscode.Uri[] = [];
+
+	private includedFiles: { [id: string]: string[] } = {};
+
+	private addError(uri: vscode.Uri, range: vscode.Range, message: string) {
+		console.log(`added error: ${message} to ${uri.path}`);
+		let diagnostics = this.diagnosticCollection.get(uri) || [];
+		const diagnostic = new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Error);
+		diagnostics = [...diagnostics, diagnostic];
+		this.diagnosticCollection.set(uri, diagnostics);
+	}
+
+	private addIncludeFile(file: string, included: string) {
+		var includes = this.includedFiles[file] || [];
+		if (includes.indexOf(included) === -1) {
+			includes.push(included);
+			this.includedFiles[file] = includes;
+		}
+		console.log(`files for ${file} : ${this.includedFiles[file]}`);
+	}
+
+	private handleEsphomeError(error: ESPHome.ValidationError) {
+		const message = error.message;
+		let range: vscode.Range;
+		let uri: vscode.Uri;
+		if (error.range === null) {
+			range = new vscode.Range(0, 0, 1, 0);
+			uri = vscode.Uri.file(this.validating!.fileName);
+		} else {
+			range = new vscode.Range(error.range.start_line, error.range.start_col, error.range.end_line, error.range.end_col);
+			uri = vscode.Uri.file(error.range.document);
+		}
+		this.addError(uri, range, message);
+	}
+
+	private handleYamlError(error: ESPHome.YamlValidationError) {
+		let skip = 3;
+		let message = '';
+		error.message.split('\n').forEach(line => {
+			console.log(`- ${skip} : ${line}`);
+			if (--skip > 0) {
+				return;
 			}
-			case 'result': {
-				console.log(`Got result message`);
-				console.log(msg);
-				let diagnostics: vscode.Diagnostic[] = [];
-				let openDocument = vscode.window.activeTextEditor!.document;
-				msg.validation_errors.forEach((error) => {
-					let message = error.message;
-					let range;
-					if (error.range === null) {
-						range = new vscode.Range(0, 0, openDocument.lineCount - 1, 0);
-					} else {
-						// Todo: Use error.range.document to add it to the correct diagnostics collection
-						range = new vscode.Range(error.range.start_line, error.range.start_col, error.range.end_line, error.range.end_col);
+			if (message === '') {
+				message = line;
+				console.log('got error ' + line);
+			}
+			else {
+				let location = line.match(/in "([^"]*)", line (\d*), column (\d*):/);
+				if (location) {
+					const uri = vscode.Uri.file(location[1]);
+					const line_number = parseInt(location[2]) - 1;
+					const col_number = parseInt(location[3]) - 1;
+					const range = new vscode.Range(line_number, col_number, line_number, col_number + 1);
+
+					this.addError(uri, range, message);
+				}
+				else {
+					console.log('unknown location ' + line);
+					console.log(location);
+				}
+				message = '';
+				skip = 3;
+			}
+		});
+	}
+
+	private handleMessage(msg: ESPHome.MessageTypes) {
+		try {
+			switch (msg.type) {
+				case 'read_file': {
+					console.log(`Got read_file message ${msg.path}`);
+					const uri = vscode.Uri.file(msg.path);
+					if (msg.path !== this.validating!.fileName) {
+						this.addIncludeFile(this.validating!.fileName, uri.path);
 					}
-					let diagnostic = new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Error);
-					diagnostics.push(diagnostic);
-				});
-				msg.yaml_errors.forEach((error) => {
-					let message = error.message;
-					// Todo: Use error.range.document to add it to the correct diagnostics collection
+					this.diagnosticCollection.delete(uri);
+					vscode.workspace.openTextDocument(uri).then((document) => {
+						let text = document.getText();
+						this.sendMsg({
+							type: 'file_response',
+							content: text,
+						});
+					}, (reason) => {
+						// won't validate as an include file is missing
+						this.addError(this.validating!.uri, new vscode.Range(0, 0, 1, 0), `Could not found '${msg.path}'`);
+						this.sendMsg({
+							type: 'file_response',
+							content: '',
+						});
+					});
+					break;
+				}
+				case 'result': {
+					console.log(`Got result message`, msg);
+					msg.validation_errors.forEach((error) => {
+						this.handleEsphomeError(error);
+					});
+					msg.yaml_errors.forEach((error) => {
 
-					let skip = 3;
-					let parse_error = '';
-					message.split('\n').forEach(line => {
-						console.log(`- ${skip} : ${line}`);
-						if (--skip > 0) {
-							return;
-						}
-						if (parse_error === '') {
-							parse_error = line;
-							console.log('got error ' + line);
-						}
-						else
-						{
-							console.log('adding diag ' + line);
-							let location = line.match(/in "([^"]*)", line (\d*), column (\d*):/);
-							if (location)
-							{
-								let line_number = parseInt(location[2]) - 1;
-								let col_number = parseInt(location[3]) - 1;
-								console.log(`parsed loc: ${location[1]}:${col_number}`);
-								let range = new vscode.Range(line_number, col_number, line_number, col_number + 1);
-								let diagnostic = new vscode.Diagnostic(range, parse_error, vscode.DiagnosticSeverity.Error);
-								diagnostics.push(diagnostic);
-							}
-							else {
-								console.log('unknown location ' + line);
-								console.log(location);
-							}
-							parse_error = '';
-							skip = 3;
-						}
+						this.handleYamlError(error);
 
 					});
+					this.validating = null;
 
-				});
-				this.diagnosticCollection.set(openDocument.uri, diagnostics);
-				this.validating = null;
-				break;
+					this.validateNext();
+					break;
+				}
+				default: {
+					console.log(`Got unknown message type`, msg);
+					break;
+				}
 			}
-			default: {
-				console.log(`Got unknown message type`);
-				console.log(msg);
-				break;
+		}
+		catch (exception) {
+			this.output_channel.appendLine(exception.toString());
+		}
+	}
+
+	private validateNext() {
+		try {
+			while (this.pendingValidation.length > 0) {
+				const doc = this.pendingValidation.pop();
+				if (!doc || !doc.path.endsWith(".yaml")) {
+					continue;
+				}
+
+				// TODO: if doc is an included don't revalidate master.
+				console.log(`validating next: ${doc.path} pending validation: ${this.pendingValidation.length}`);
+				vscode.workspace.openTextDocument(doc).then(openedTextDocument =>
+					this.doLint(openedTextDocument));
+
+				return;
 			}
+		}
+		catch (exception) {
+			this.output_channel.appendLine(exception.toString());
 		}
 	}
 
@@ -163,9 +183,7 @@ export default class EsphomeProvider {
 		}
 		else {
 			this.output_channel.appendLine("Using local ESPHome");
-
 			this.process = ChildProcess.spawn('esphome', ['dummy', "vscode"]);
-
 			this.process.stdout.on('data', (data) => {
 				console.log('Got out: ' + data.toString());
 				const msg = JSON.parse(data);
@@ -178,17 +196,16 @@ export default class EsphomeProvider {
 			});
 		}
 
-
 		vscode.workspace.onDidOpenTextDocument(this.doLint, this, subscriptions);
 		vscode.workspace.onDidSaveTextDocument(this.doLint, this);
-		// TODO: Activate linting when changing
-		// Currently this causes issues because the client is not implemented
-		// thread-safe
 		vscode.workspace.onDidChangeTextDocument((evt) => {
 			this.doLint(evt.document);
 		}, this);
 
-		vscode.workspace.textDocuments.forEach(this.doLint, this);
+		vscode.workspace.findFiles('*.yaml').then((uris: vscode.Uri[]) => {
+			this.pendingValidation = uris;
+			this.validateNext();
+		});
 	}
 
 	public dispose(): void {
@@ -228,28 +245,36 @@ export default class EsphomeProvider {
 		if (textDocument.languageId !== 'yaml') {
 			return;
 		}
-		if (textDocument.fileName.endsWith('secrets.yaml')) {
-			return;
-		}
-
 		if (this.validating !== null) {
 			return;
 		}
-		this.validating = textDocument.fileName;
-		// TODO: The open file may not be the file we want to validate
-		// For example when editing an !included file.
-		console.log(`Validating ${textDocument.fileName}`);
+
+		this.validating = textDocument;
+		// Check if this is an included file
+		const thisFilePath = vscode.Uri.file(textDocument.fileName).path;
+		console.log(`this file path: ${thisFilePath}`);
+		for (let key in this.includedFiles) {
+			console.log(`testing included file key: ${key} files: ${this.includedFiles[key]}`);
+			if (this.includedFiles[key].indexOf(thisFilePath) >= 0) {
+				console.log(`Not validating ${textDocument.fileName} as is listed as included file`);
+				const doc = vscode.workspace.textDocuments.find(td => td.fileName === key);
+				if (doc !== undefined) {
+					console.log(`Validating containing document ${doc.uri.path} instead`);
+					this.validating = doc;
+				}
+			}
+		}
+
+		console.log(`Validating ${this.validating.fileName}`);
 
 		this.sendMsg({
 			type: 'validate',
-			file: textDocument.fileName
+			file: this.validating.fileName
 		});
 	}
 }
 
 export function activate(ctx: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "esphome-vscode" is now active!');
-
 	let linter = new EsphomeProvider();
 	linter.activate(ctx.subscriptions);
 }
