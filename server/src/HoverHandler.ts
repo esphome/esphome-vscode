c901f6a25634db58ea9b88684646a787e0804b1d
Guillermo Ruffino <glm.net@gmail.com>
2022-04-01 00:32:02 -0300

    don't throw error on promise

--- server/src/HoverHandler.ts
+++ server/src/HoverHandler.ts
@@ -28,17 +28,6 @@ export class HoverHandler {
         catch (error) {
             console.log('yaml.hover.error', { error, documentUri: textDocument.uri });
         }
-
-
-        return {
-            contents: {
-
-                kind: "markdown",
-                value: "Hello from hover"
-
-            }
-
-        };
     }
 
     getPath(node: YamlNode, currentDoc: SingleYAMLDocument) {
@@ -114,90 +103,97 @@ export class HoverHandler {
         let pathNode;
         pathNode = docNode;
 
+
+
         return new Promise<Hover>((resolve, reject) => {
 
+            try {
 
-            var path = this.getPath(node, doc);
+                var path = this.getPath(node, doc);
 
-            let schema: Schema;
-            let cv: ConfigVar;
+                let schema: Schema;
+                let cv: ConfigVar;
 
-            if (path.length === 1) {
-                const rootComponents = this.coreSchema.getComponentList();
-                if (path[0] in rootComponents) {
-                    return resolve(createHover(rootComponents[path[0]].docs));
+                if (path.length === 1) {
+                    const rootComponents = this.coreSchema.getComponentList();
+                    if (path[0] in rootComponents) {
+                        return resolve(createHover(rootComponents[path[0]].docs));
+                    }
+                    const platformComponents = this.coreSchema.getPlatformList();
+                    if (path[0] in platformComponents) {
+                        return resolve(createHover(platformComponents[path[0]].docs));
+                    }
+
+                    return resolve(null);
                 }
-                const platformComponents = this.coreSchema.getPlatformList();
-                if (path[0] in platformComponents) {
-                    return resolve(createHover(platformComponents[path[0]].docs));
-                }
 
-                return resolve(null);
-            }
-
-            for (let index = 0; index < path.length; index++) {
-                if (isString(path[index]) && isMap(pathNode)) {
-                    if (cv === undefined && index <= 2 && pathNode.get("platform")) {
-                        const componentName = pathNode.get("platform");
-                        if (isString(componentName)) {
-                            const platformComponents = this.coreSchema.getPlatformList();
-                            if (path[0] in platformComponents) {
-                                const c = this.coreSchema.getComponent(path[0]);
-                                if (c.components !== undefined && componentName in c.components) {
-                                    cv = this.coreSchema.getComponentPlatformSchema(componentName, path[0]);
+                for (let index = 0; index < path.length; index++) {
+                    if (isString(path[index]) && isMap(pathNode)) {
+                        if (cv === undefined && index <= 2 && pathNode.get("platform")) {
+                            const componentName = pathNode.get("platform");
+                            if (isString(componentName)) {
+                                const platformComponents = this.coreSchema.getPlatformList();
+                                if (path[0] in platformComponents) {
+                                    const c = this.coreSchema.getComponent(path[0]);
+                                    if (c.components !== undefined && componentName in c.components) {
+                                        cv = this.coreSchema.getComponentPlatformSchema(componentName, path[0]);
+                                    }
                                 }
                             }
                         }
-                    }
 
-                    pathNode = pathNode.get(path[index], true);
-                    if (cv === undefined) {
-                        const rootComponents = this.coreSchema.getComponentList();
-                        if (path[0] in rootComponents) {
-                            cv = this.coreSchema.getComponent(path[0]).schemas.CONFIG_SCHEMA;
-                        }
-                    }
-                    else {
-                        if (cv.type === "schema" || cv.type === "trigger") {
-                            if (cv.schema !== undefined) {
-                                const schema_cv = this.coreSchema.findConfigVar(cv.schema, path[index], docNode);
-                                if (schema_cv !== undefined) {
-                                    cv = schema_cv;
-                                    continue;
-                                }
-                            }
-
-                            if (cv.type === "trigger") {
-                                const action = this.coreSchema.getActionConfigVar(path[index]);
-                                if (action !== undefined) {
-                                    cv = action;
-                                    continue;
-                                }
+                        pathNode = pathNode.get(path[index], true);
+                        if (cv === undefined) {
+                            const rootComponents = this.coreSchema.getComponentList();
+                            if (path[0] in rootComponents) {
+                                cv = this.coreSchema.getComponent(path[0]).schemas.CONFIG_SCHEMA;
                             }
                         }
-                        if (cv.type === "registry") {
-                            cv = this.coreSchema.getRegistryConfigVar(cv.registry, path[index]);
+                        else {
+                            if (cv.type === "schema" || cv.type === "trigger") {
+                                if (cv.schema !== undefined) {
+                                    const schema_cv = this.coreSchema.findConfigVar(cv.schema, path[index], docNode);
+                                    if (schema_cv !== undefined) {
+                                        cv = schema_cv;
+                                        continue;
+                                    }
+                                }
+
+                                if (cv.type === "trigger") {
+                                    const action = this.coreSchema.getActionConfigVar(path[index]);
+                                    if (action !== undefined) {
+                                        cv = action;
+                                        continue;
+                                    }
+                                }
+                            }
+                            if (cv.type === "registry") {
+                                cv = this.coreSchema.getRegistryConfigVar(cv.registry, path[index]);
+                            }
                         }
                     }
+                    else if (isNumber(path[index]) && isSeq(pathNode)) {
+                        pathNode = pathNode.get(path[index], true);
+                    }
                 }
-                else if (isNumber(path[index]) && isSeq(pathNode)) {
-                    pathNode = pathNode.get(path[index], true);
+
+                let content: string;
+                if (cv !== undefined) {
+                    content = cv.docs;
+                } else {
+                    content = JSON.stringify(schema);
+
+                    if (schema["docs"] !== undefined) {
+                        content = schema["docs"];
+                    }
                 }
+
+                const hover = createHover(content);
+                resolve(hover);
             }
-
-            let content: string;
-            if (cv !== undefined) {
-                content = cv.docs;
-            } else {
-                content = JSON.stringify(schema);
-
-                if (schema["docs"] !== undefined) {
-                    content = schema["docs"];
-                }
+            catch (ex) {
+                console.log(ex);
             }
-
-            const hover = createHover(content);
-            resolve(hover);
         });
     }
 }
